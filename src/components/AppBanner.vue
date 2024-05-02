27af9bb6a084869c92c2a7b9ad94db7efba0e247
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-02 19:44:52 +0200

    add: content&style - AppBanner

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -2,7 +2,43 @@
 
 export default {
 
+  data() {
+    return {
 
+      menu: [
+        {
+          category: "DIGITAL COMICS",
+          icon: "comics-digital-comics.png",
+        },
+        {
+          category: "DC MERCHANDISE",
+          icon: "comics-merchandise.png",
+        },
+        {
+          category: "SUBSCRIPTION",
+          icon: "comics-subscriptions.png",
+        },
+        {
+          category: "COMIC SHOP LOCATOR",
+          icon: "comics-shop-locator.png",
+        },
+        {
+          category: "DC POWER VISA",
+          icon: "dc-power-visa.svg",
+        },
+      ],
+
+    };
+  },
+
+  methods: {
+
+    getImagePath(imageName) {
+
+      return new URL(`../assets/img/buy-${imageName}`, import.meta.url).href;
+
+    },
+  },
 
 };
 
@@ -12,14 +48,66 @@ export default {
 
   <div id="banner-container">
 
-    <h2>Banner</h2>
+    <div class="wrapper">
+
+      <a v-for="element in menu">
+
+        <img :src="getImagePath(element.icon)" alt="">
+
+        <h3> {{ element.category }}</h3>
+
+      </a>
+
+    </div>
 
   </div>
-  
+
 </template>
 
 <style lang="scss" scoped>
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
 
+#banner-container {
+  @include flex(row, space-between, center);
+  color: $general-white;
+  background-color: $general-lightblue;
+  padding: 50px 0;
 
+  // Debug
+  // height: 250px;
 
+  .wrapper {
+    @include flex(row, space-between, center);
+
+    a {
+      @include flex(row, space-between, center);
+
+      img {
+        height: 50px;
+        
+
+      }
+
+      &:last-child {
+
+        img {
+          height: auto;
+          width: 50px;
+
+        }
+
+      }
+
+      h3 {
+        font-size: small;
+        padding-left: 10px;
+
+      }
+
+    }
+
+  }
+
+}
 </style>
