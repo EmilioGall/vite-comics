f03b155191360a470a0ca3add51a2d9ed64a3625
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-02 16:58:38 +0200

    add: reset - general - scss

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,54 @@
 
 export default {
 
+  data() {
+    return {
 
+      menu: [
+        {
+          title: "CHARACTERS",
+          isActive: false,
+        },
+        {
+          title: "COMICS",
+          isActive: true,
+        },
+        {
+          title: "MOVIES",
+          isActive: false,
+        },
+        {
+          title: "TV",
+          isActive: false,
+        },
+        {
+          title: "GAMES",
+          isActive: false,
+        },
+        {
+          title: "COLLECTIBLES",
+          isActive: false,
+        },
+        {
+          title: "VIDEOS",
+          isActive: false,
+        },
+        {
+          title: "FANS",
+          isActive: false,
+        },
+        {
+          title: "NEWS",
+          isActive: false,
+        },
+        {
+          title: "SHOP",
+          isActive: false,
+        },
+      ],
+
+    };
+  },
 
 };
 
@@ -10,12 +57,30 @@ export default {
 
 <template>
 
-  <h1>Header</h1>
+  <header>
+
+    <div>
+
+      <img src="" alt="">
+
+    </div>
+
+    <div>
+
+      <ul>
+
+        <li v-for="element in menu">
+
+          {{ element.title }}
+
+        </li>
+
+      </ul>
+
+    </div>
+
+  </header>
 
 </template>
 
-<style lang="scss" scoped>
-
-
-
-</style>
+<style lang="scss" scoped></style>
